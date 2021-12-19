8db291ea20c06a88b5469c9a188bc88c8fd7f381
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-19 12:07:56 -0600

    add working index route for player headlines. still need to update dynamic params.

--- app/controllers/player_news_headlines_controller.rb
+++ app/controllers/player_news_headlines_controller.rb
@@ -1,2 +1,6 @@
 class PlayerNewsHeadlinesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?q=NFL&country=us&category=sports&apiKey=#{Rails.application.credentials.newsapi}")
+    render json: response.parse
+  end
 end
