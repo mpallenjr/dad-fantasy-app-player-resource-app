feaf204217416a01abccfb64ba618611b6931cff
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-19 20:56:00 -0600

    prep for upload to heroku.

--- app/controllers/player_news_headlines_controller.rb
+++ app/controllers/player_news_headlines_controller.rb
@@ -2,21 +2,12 @@ class PlayerNewsHeadlinesController < ApplicationController
   def index
     dt = Date.parse(Date.today.to_s)
     dy = Date.parse(Date.yesterday.to_s)
-    # d.strftime('%Y %b %d')
-    # first_name = params[:first_name]
-    # last_name = params[:last_name]
-    # twitter_username = params[:twitter_username]
+ 
     first_name = params[:first_name]
     last_name = params[:last_name]
-    # full_name = "#{first_name} - #{last_name}"
-
-    # from = Time.new - 1.day
-    # till = Time.new
-
-
+  
     response = HTTP.get("https://newsapi.org/v2/everything?from=#{dy}&to=#{dt}&q=#{first_name}-#{last_name}&sortBy=popularity&apiKey=#{Rails.application.credentials.newsapi}")
     render json: response.parse
-
-
+    
   end
 end
--- app/controllers/twitter_username_search_results_controller.rb
+++ app/controllers/twitter_username_search_results_controller.rb
@@ -1,5 +1,6 @@
 class TwitterUsernameSearchResultsController < ApplicationController
   def index
+    
     response = HTTP.auth("Bearer #{Rails.application.credentials.twitterapi}").get("https://api.twitter.com/2/tweets/search/recent?query=fantasy%20basketball%20dfs%20-is%3Aretweet")
 
     render json: response.parse
