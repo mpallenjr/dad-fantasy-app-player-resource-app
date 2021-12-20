ea10416a7124871e5dcfcfa9cd0da7fbb25dd6df
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-19 21:06:50 -0600

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'https://blissful-yalow-a3f18c.netlify.app/'
 
     resource '*',
       headers: :any,
